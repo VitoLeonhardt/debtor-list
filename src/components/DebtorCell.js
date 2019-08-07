a2a0ed082b6247658eb0c0f7c47d8646d560d651
VitoLeonhart <vito.leonhart@gmail.com>
2019-08-07 20:07:18 +0200

    Made the data display in a table

--- src/components/DebtorCell.js
+++ src/components/DebtorCell.js
@@ -1,9 +1,18 @@
 // @flow
 
 import React from "react";
+import { Table } from "semantic-ui-react";
 
 import type { DebtorT } from "../misc/Types";
 
 export const DebtorCell = (props: DebtorT) => { 
-    return <div>{props.name}</div>
+    const { name, date_of_first_purchase, amount, amount_left } = props;
+    return (<Table.Row>
+        <Table.Cell>{name}</Table.Cell>
+        <Table.Cell>{date_of_first_purchase}</Table.Cell>
+        <Table.Cell>{amount}</Table.Cell>
+        <Table.Cell>{amount - amount_left}</Table.Cell>
+        <Table.Cell>{amount_left}</Table.Cell>
+      </Table.Row>)
+
 }
--- src/components/DebtorList.js
+++ src/components/DebtorList.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { Table } from "semantic-ui-react";
 
 import { LoadDebtors } from "../redux/actions";
 import { DebtorCell } from "./DebtorCell";
@@ -14,6 +15,19 @@ export const DebtorList = () => {
         }
     })
     return <div>
+    <Table celled selectable>
+        <Table.Header>
+            <Table.Row>
+                <Table.HeaderCell>Name</Table.HeaderCell>
+                <Table.HeaderCell>Date of first purchase</Table.HeaderCell>
+                <Table.HeaderCell>Max amount</Table.HeaderCell>
+                <Table.HeaderCell>Amount used</Table.HeaderCell>
+                <Table.HeaderCell>Amount left</Table.HeaderCell>
+            </Table.Row>
+        </Table.Header>
+        <Table.Body>
     { debtors.map((debtor: DebtorT) => <DebtorCell {...debtor} />) }
+    </Table.Body>
+    </Table>
     </div>
 }
